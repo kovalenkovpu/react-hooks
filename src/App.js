6189dbd8cbf109d835874c633986b435aedc0a78
Pavel Kovalenkov <pavel_kovalenkov@epam.com>
2019-09-23 17:17:32 +0300

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState({ times: 0 });
+  const onCountClick = () => {
+    return setCount({ times: count.times + 10 });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count.times} times</p>
+      <button onClick={onCountClick}>
+        Click me
+      </button>
     </div>
   );
 }
